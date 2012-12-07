02401a0766f201fea0a3e654e42ec5cdb6e81828
Arun Kumar <arunfairy@hotmail.com>
2012-12-07 22:32:18 +0530

    Version 1.1

--- js/photoZoom.js
+++ js/photoZoom.js
@@ -1,15 +1,21 @@
+/*
+ * Version  : 1.1
+ * Author   : Arun Kumar Sekar
+ * Mail     : arunkumarsekar@hotmail.com
+ * Plugin   : photoZoom.js
+ */
 (function($){
     $.fn.photoZoom = function(options){
         var defaults = {
             onMouseOver   : function(){},        // callback on mouse hover
             zoomStyle     : {"background-color"   : "#fff",
-                             "min-width"          : "50px",
-                             "min-height"         : "50px",
-                             "border"             : "1px solid #ccc",
-                             "-moz-box-shadow"    : "0 0 5px #888",
-                             "box-shadow"         : "0 0 5px #888",
-                             "-webkit-box-shadow" : "0 0 5px #888",
-                             "text-align"         : "center"},
+                "min-width"          : "50px",
+                "min-height"         : "50px",
+                "border"             : "1px solid #ccc",
+                "-moz-box-shadow"    : "0 0 5px #888",
+                "box-shadow"         : "0 0 5px #888",
+                "-webkit-box-shadow" : "0 0 5px #888",
+                "text-align"         : "center"},
             onMouseOut    : function(){}        // callback on mouse out
         };
         $this = $(this);
@@ -21,6 +27,8 @@
             obj         : null,
             maxWidth    : 0,
             divObj      : $this.coreObj,
+            winWidth    : jQuery(window).width(),
+            winHeight   : jQuery(window).height(),
 
             init : function(opt){
                 this.obj = opt.obj;
@@ -72,33 +80,42 @@
                 tmpImg.src = jQuery(imgObj).attr("src");
                 var w = 0, h = 0,maxWidth = 0,maxHeight = 0,leftPos = 0,topPos = 0,rightPos = 0, x = 0, y = 0;
 
-                w = jQuery(window).width();
-                h = jQuery(window).height();
+                w = this.winWidth;
+                h = this.winHeight;
                 x = e.pageX;
                 y = e.pageY;
 
                 var ratio = (w / 100 * 55);
                 topPos = (y+10);
+                var imgObj = this.divObj.find("img");
+
                 if(x > ratio){
                     // Right position
                     maxWidth = (tmpImg.width < (x-20)) ? tmpImg.width : (x-20);
                     rightPos = (x+10) - (tmpImg.width > (w-x))? (w-x) : tmpImg.width;
-                    this.divObj.find("img").css("height",imgHeight+"px");
-                    this.divObj.css({"width":maxWidth,
-                                     "height": "auto",
-                                     "left":"",
-                                     "right":rightPos+"px",
-                                     "top": topPos+"px" });
+                    imgObj.css("max-width",maxWidth+"px");
+                    this.divObj.css({"left":"",
+                        "right":rightPos+"px",
+                        "top": topPos+"px" });
                 } else {
                     // Left Position
                     maxWidth = (tmpImg.width > (w-(x+20)))? (w-(x+20)) : tmpImg.width;
-                    var imgHeight = (tmpImg.height / tmpImg.width * maxWidth);
-                    this.divObj.css({"width":maxWidth,
-                                     "height":"auto",
-                                     "left":(x+10)+"px",
-                                     "right":"",
-                                     "top": topPos+"px" });
+                    imgObj.css("max-width",maxWidth+"px");
+                    this.divObj.css({"left":(x+10)+"px",
+                        "right":"",
+                        "top": topPos+"px" });
                 }
+
+                /* Bottom Layer */
+                var hRatio = (h / 100 * 55);
+                if(hRatio > y){
+                    maxHeight = ((h - y) - 10);
+                } else {
+                    this.divObj.css({"top":(topPos - (imgObj.height() + 20))+"px"});
+                    maxHeight = ((y - h) - 10);
+                }
+                maxHeight = (this.winHeight < maxHeight) ? this.winHeight : maxHeight;
+                imgObj.css({'max-height':maxHeight+"px"});
             },
             whenHoverOut:function(imgObj){
                 this.divObj.hide();
@@ -107,13 +124,13 @@
             wrapResponsive:function(){
                 $newImage = jQuery("<img />");
                 $newImage.css({"max-width"  :"100%",
-                               "position"   :"relative",
-                               "padding"    :"0px",
-                               "margin-top" :"40%"})
-                         .attr("src","images/zoomLoader.gif");
+                    "position"   :"relative",
+                    "padding"    :"0px",
+                    "margin-top" :"40%"})
+                    .attr("src","images/zoomLoader.gif");
                 $newImage = jQuery("<div class='photoZoom-Large'></div>")
-                            .css($.extend($this.options.zoomStyle,{ "display": "none","position": "absolute"}))
-                            .html($newImage);
+                    .css($.extend($this.options.zoomStyle,{ "display": "none","position": "absolute"}))
+                    .html($newImage);
                 return $newImage;
             },
             images:function(src){
